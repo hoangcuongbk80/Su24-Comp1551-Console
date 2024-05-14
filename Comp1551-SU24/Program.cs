02e0075548be304c9b53018548f6d96eb3d8aee1
Cuong Hoang~ <hoangcuongbk80@gmail.com>
2024-05-14 08:41:13 +0700

    intial commit

---
+++ Comp1551-SU24/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Comp1551_SU24
+{
+    internal class Program
+    {
+        static void printHello()
+        {
+            Console.WriteLine("PrintHello without parameters");
+        }
+        static void printHello(string str)
+        {
+            Console.WriteLine(str);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, CO1103!");
+            printHello();
+            printHello("Cuong");
+        }
+    }
+}
