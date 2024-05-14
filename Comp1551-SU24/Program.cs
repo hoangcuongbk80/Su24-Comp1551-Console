5b1ad39228204892e732fb76c6249a8addf88c12
Cuong Hoang~ <hoangcuongbk80@gmail.com>
2024-05-14 12:08:49 +0700

    intial commit

--- Comp1551-SU24/Program.cs
+++ Comp1551-SU24/Program.cs
@@ -22,9 +22,10 @@
             // printHello();
             // printHello("Cuong");
 
-            Console.Write("Enter an integer number: ");
-            double number = Convert.ToDouble(Console.ReadLine());
-            if(number > 0)
+            /*Console.Write("Enter an integer number: ");
+            double number; //= Convert.ToDouble(Console.ReadLine());
+            number = Convert.ToDouble(Console.ReadLine());
+            if (number > 0)
             {
                 double result = SquareRoot(number);
                 Console.WriteLine("Square root of {0} is: {1:F2}", number, result);
@@ -32,7 +33,22 @@
             else
             {
                 Console.WriteLine("Cannot find square root of a negative number");
-            }
+            }*/
+
+            double a1 = 3.0;
+            double b1 = 0.000000000000000000001;
+            double c1 = a1 + b1;
+            Console.WriteLine(c1);
+
+            Decimal a2 = 3.0M;
+            Decimal b2 = 0.000000000000000000001M;
+            Decimal c2 = a2 + b2;
+            Console.WriteLine(c2);
+
+            var a3 = 3.0;
+            var b3 = 0.000000000000000000001;
+            var c3 = a3 + b3;
+            Console.WriteLine(c3);
         }
     }
 }
--- Comp1551-SU24/obj/Debug/net8.0/Comp1551-SU24.AssemblyInfo.cs
+++ Comp1551-SU24/obj/Debug/net8.0/Comp1551-SU24.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Comp1551-SU24")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38af9d895e48e70184c31697fdf47657548ff5e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e67abf7d133b6e5787652f3a492ebd49f5531e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Comp1551-SU24")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Comp1551-SU24")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
