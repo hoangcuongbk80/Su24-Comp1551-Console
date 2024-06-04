d7e736dfe0081659df186784f161812ee8804ff9
Cuong Hoang~ <hoangcuongbk80@gmail.com>
2024-06-04 10:33:29 +0700

    lecture 4

--- Lecture3/obj/Debug/net8.0/Lecture3.AssemblyInfo.cs
+++ Lecture3/obj/Debug/net8.0/Lecture3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lecture3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c184d8b1d17f350439eed63f20086a152a2f328")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+227c22fdcfd09e59614f1901cafac9abaf5c3624")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lecture3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lecture3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lecture4/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace Lecture4
+{
+    class MonthName
+    {
+        private int month = 1;
+        public int Month
+        {
+            get { return month; }
+            set { if((value>0) && (value<13)) { month = value; }}
+        }
+    }
+
+    class QuadraticEquation
+    {
+        private double a;
+        private double b;
+        private double c;
+        private double D;
+        private double? x1;
+        private double? x2;
+
+        public double? X1 {
+            get { return x1; } set { x1 = value; }
+        }
+        public double? X2
+        {
+            get { return x2; }
+            set { x2 = value; }
+        }
+
+        public void Calculate(double p1, double p2, double p3)
+        {
+            a = p1; b = p2; c = p3;
+            D = Convert.ToDouble(b * b - 4 * a * c);
+            if((a!=0) && (D >= 0))
+            {
+                x1 = Convert.ToDouble((-b + Math.Sqrt(D)) / (2*a));
+                x2 = Convert.ToDouble((-b - Math.Sqrt(D)) / (2*a));
+            }
+            else
+            {
+                x1 = null;
+                x2 = null;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------Lecture 4----------------");
+
+            /*String S;
+            System.Globalization.DateTimeFormatInfo Months = new System.Globalization.DateTimeFormatInfo();
+            MonthName a = new MonthName();
+            a.Month = 3;
+            S= Convert.ToString(Months.GetMonthName(a.Month));
+            Console.WriteLine("The month is: {0}", S);*/
+
+            QuadraticEquation quadraticEquation = new QuadraticEquation();
+            quadraticEquation.Calculate(27,4,-5);
+            if (quadraticEquation.X1 != null)
+            {
+                Console.WriteLine("Result: {0}, {1}", quadraticEquation.X1, quadraticEquation.X2);
+            }
+            else
+            {
+                Console.WriteLine("Result: null");
+            }
+
+
+        }
+    }
+}
